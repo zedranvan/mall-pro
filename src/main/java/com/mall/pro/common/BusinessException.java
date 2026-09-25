bc926a02a5f00e9404f1089a7e37818c09ac62ce
Chujibingpu <250072316+zedranvan@users.noreply.github.com>
2026-09-25 19:19:53 +0800

    增加对应异常处理已经部分实验代码

---
+++ src/main/java/com/mall/pro/common/BusinessException.java
@@ -0,0 +1,14 @@
+package com.mall.pro.common;
+
+public class BusinessException extends RuntimeException {
+    private final Integer code;
+    public  BusinessException( String message) {
+        super(message);
+        this.code = 400;
+    }
+
+    public  BusinessException( Integer code, String message) {
+        super(message);
+        this.code = code;
+    }
+}
